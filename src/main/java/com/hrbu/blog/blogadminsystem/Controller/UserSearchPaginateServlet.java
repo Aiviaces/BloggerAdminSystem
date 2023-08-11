a724e9789be0e0f91f3776770170781e27560a66
Aiviaces <aiviaces@163.com>
2023-08-11 11:29:56 +0800

    前端更新 用户操作页 -- 分页查询基本完工

--- src/main/java/com/hrbu/blog/blogadminsystem/Controller/UserSearchPaginateServlet.java
+++ src/main/java/com/hrbu/blog/blogadminsystem/Controller/UserSearchPaginateServlet.java
@@ -7,10 +7,12 @@ import com.hrbu.blog.blogadminsystem.Util.CustomExceptions.WebParamIllegalExcept
 import com.hrbu.blog.blogadminsystem.Util.WebRequestUtil;
 import com.hrbu.blog.blogadminsystem.Util.WebResponsUtil;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @WebServlet(name = "UserSearchPaginateServlet", value = "/UserSearchPaginateServlet")
